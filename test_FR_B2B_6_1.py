60cbe72335823a3046de4ddbd3480dd6b18c7b3a
Manos <man.adamopoulos@gmail.com>
2023-04-21 09:51:52 +0300

    corrections after platform changes

--- test_FR_B2B_6_1.py
+++ test_FR_B2B_6_1.py
@@ -7,11 +7,18 @@ def test_case_id_16(playwright: Playwright) -> None:
     config = configparser.ConfigParser()
     config.read('config.env', 'utf-8')
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    # context = browser.new_context()
+    context = browser.new_context(
+        http_credentials={"username": "lbUser", "password": "lbP4ss2022!"}
+    )
     page = context.new_page()
-    page.goto(config['PAGE']['UrlB2B'])
+    page.goto(config['PAGE']['Url'])
+
+    page.get_by_text("B2B").click()
+    page.get_by_role("button", name="Είσοδος στην Πλατφόρμα").click()
+
     page.get_by_placeholder("Αναζήτηση Αγοράς...").click()
-    page.get_by_role("option", name="Ξενοδοχεία" + " " + "(" + config['EXAMPLES']['b2bHotCnt_1'] + ")").click()
+    page.get_by_role("option", name=config['EXAMPLES']['example_market']).click()
     page.get_by_role("button", name="Αναζήτηση").click()
 
     page.locator(f"[id=\"\\30 {int(config['EXAMPLES']['v4'])}\"]").get_by_text(
